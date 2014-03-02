1bb9b37bb92a83ca8cd5fabdf2e50f8290dd8484
Matt Bates <BatesMatthewJ@GMail.com>
2014-03-01 22:57:55 -0500

    Added service skeleton

---
+++ CodeNameImpregnable/src/com/vcu/codenameimpregnable/PBumpService.java
@@ -0,0 +1,113 @@
+package com.vcu.codenameimpregnable;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class PBumpService extends Service{
+
+	boolean started = false;
+	
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return null;
+	}
+	
+	@Override
+	public void onStart(Intent intent, int startId) {
+	   // handleCommand(intent);
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+	    // We want this service to continue running until it is explicitly
+	    // stopped, so return sticky.
+		
+		if(!started){
+			started = true;
+			
+			//start listening
+			startListening();
+			
+			//look at closest device
+			lookAtDevices();
+			//continued in lookAtDevices
+				// check for trigger
+			
+					//send trigger data
+					
+					//listen for trigger data
+			
+						//send all data
+			
+						//receive data
+				
+						//email/sms
+			
+		}
+	    return START_STICKY;
+	}
+
+	private void lookAtDevices() {
+		
+		//do this in another thread
+
+		// check for trigger
+		if(isTriggerConditionMet()){
+			//send trigger data
+			sendTriggerData();
+
+			//listen for trigger data
+			listenForTriggerData();
+			//continued in listenForTriggerData
+
+				//send all data
+
+				//receive data
+		
+				//email/sms
+		}
+	}
+
+	private void listenForTriggerData() {
+		sendAllData();
+		//send all data
+
+		receiveData();
+		//receive data
+
+		sendEmailSms();
+		//email/sms
+	}
+
+	private void sendEmailSms() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void receiveData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void sendAllData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void sendTriggerData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private boolean isTriggerConditionMet() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private void startListening() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
