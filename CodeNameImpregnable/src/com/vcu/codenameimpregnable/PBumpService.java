0c0c16893316c81bd363c9c98edcdc8c9b7cf357
Blackchick <mirabilesp@vcu.edu>
2014-03-02 00:17:07 -0500

    A lot of shit added
    setTemplate()
    private class SendEmailTask extends AsyncTask
    sendEmailSms()

--- CodeNameImpregnable/src/com/vcu/codenameimpregnable/PBumpService.java
+++ CodeNameImpregnable/src/com/vcu/codenameimpregnable/PBumpService.java
@@ -1,9 +1,20 @@
 package com.vcu.codenameimpregnable;
-
+import com.github.sendgrid.SendGrid;
 import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.IBinder;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PBumpService extends Service{
 
@@ -11,6 +22,12 @@ public class PBumpService extends Service{
 	BluetoothAdapter bt_adapter;
 	String data_to_send;
 	String data_recieved;
+	String addTo,setFrom,setSubject,setText,phoneNumber;
+	
+	// CREATING FIELDS FOR BATES I MEAN TO SEND SHIT
+	
+	
+	
 	
 	@Override
 	public IBinder onBind(Intent arg0) {
@@ -79,14 +96,34 @@ public class PBumpService extends Service{
 
 		receiveData();
 		//receive data
-
+		setTemplate("mirabilesp@vcu.edu","mirabilesp@vcu.edu",
+				     "Testing Application", "If you got this, then we will make trillions",null);
 		sendEmailSms();
 		//email/sms
 	}
 
 	private void sendEmailSms() {
 		// TODO Auto-generated method stub
+		SendGrid sendgrid = new SendGrid("mirabile", "xavier131");
 		
+		if (phoneNumber != null){
+			
+			sendgrid.addTo(addTo);
+			sendgrid.setFrom(setFrom);
+			sendgrid.setSubject(setSubject);
+			sendgrid.setText(setText);
+
+			new SendEmailTask().execute(sendgrid);
+			
+		}else{
+			
+			phoneNumber = "+1"+phoneNumber;
+			
+			SmsManager smsManager = SmsManager.getDefault();
+		
+			smsManager.sendTextMessage(phoneNumber, null, setText, null, null);
+			
+		}
 	}
 
 	private void receiveData() {
@@ -114,4 +151,35 @@ public class PBumpService extends Service{
 		
 	}
 	
-}
+	private void setTemplate(String addTo, String setFrom,
+			String setSubject, String setText, String phoneNumber){
+		
+		this.addTo = addTo;
+		this.setFrom = setFrom;
+		this.setSubject = setSubject;
+		this.setText = setText;
+		this.phoneNumber = phoneNumber;
+		
+	}
+	
+	
+	private class SendEmailTask extends AsyncTask<SendGrid, Integer, Long>{
+		protected Long doInBackground(SendGrid... grids){
+			grids[0].send();
+			Log.d("ERROR: ","We sent the Emails!");
+			
+			return (long) 0;
+		}
+		
+		protected void onProgressUpdate(Integer... progress){
+			//setProgressPercent(progress[0]);
+		}
+		
+		protected void onPostExecute(Long result){
+			//
+		}
+		
+	}// end Class SendEmailTask
+
+	
+}// end Class PBumbServices
