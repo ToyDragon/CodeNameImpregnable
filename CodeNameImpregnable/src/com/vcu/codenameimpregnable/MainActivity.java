33aa1875daebbd9edce3945427c02c6a291cc539
Matt Bates <BatesMatthewJ@GMail.com>
2014-03-02 03:56:36 -0500

    Moved timing of stopping service

--- CodeNameImpregnable/src/com/vcu/codenameimpregnable/MainActivity.java
+++ CodeNameImpregnable/src/com/vcu/codenameimpregnable/MainActivity.java
@@ -33,8 +33,6 @@ public class MainActivity extends Activity {
 
 	public void onPause(){
 		super.onPause();
-
-		stopService(pbump_service_intent);
 		
 		BluetoothAdapter bt_adapter = BluetoothAdapter.getDefaultAdapter();
 		if(bt_adapter!=null){
@@ -52,5 +50,10 @@ public class MainActivity extends Activity {
 		if(bt_adapter.getName().indexOf("PBump-")==0){
 			bt_adapter.setName(bt_adapter.getName().substring("PBump-".length()));
 		}
+	}
+	
+	public void onDestroy(){
+		stopService(pbump_service_intent);
+		super.onDestroy();
 	}
 }
